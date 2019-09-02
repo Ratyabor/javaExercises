f929c184b168fb8a252b83e43b62a910656d6008
Ratibor <ratyabor@gmail.com>
2019-09-02 17:58:58 +0300

    - Add exercise 10

--- src/main/java/exercise10/exercise10.java
+++ src/main/java/exercise10/exercise10.java
@@ -4,6 +4,13 @@ public class exercise10 {
 
     public static void main(String[] args) {
 
+        exercise10.Cat cat1 = new exercise10.Cat();
+        Cat.count++;
+        exercise10.Cat cat2 = new exercise10.Cat();
+        Cat.count++;
+
+        System.out.println("Created classes: " + Cat.count++);
+
     }
 
     public static class Cat {
